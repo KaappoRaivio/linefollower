74c3a5b5265ff7f5f30728519f8a5c8ec5c736f5
NoSocksForYou <kaappo.raivio@gmail.com>
2018-10-21 15:38:39 +0300

    autocommit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 print("trying to get motor handle")
 motor = ev3.Motor("outA")
 print("got ")
-motor.run_forever()
-time.sleep(1)
-motor.stop()
+# motor.run_forever()
+m = ev3.Motor('outA')
+
+m.run_to_rel_pos(position_sp=360, speed_sp=900, stop_action="hold")
+time.sleep(5)
