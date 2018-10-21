96028bf343dbdbe288e2f94a1d4b3c25132fc41f
NoSocksForYou <kaappo.raivio@gmail.com>
2018-10-21 15:47:04 +0300

    autocommit

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ print("trying to get motor handle")
 
 # motor.run_forever()
 m = ev3.Motor('outA')
-m.run_forever(speed_sp=900)
+m.run_forever(speed_sp=500)
+
 time.sleep(5)
+m.run_forever(speed_sp=1000)
+
 m.stop(stop_action="coast")
 time.sleep(5)
