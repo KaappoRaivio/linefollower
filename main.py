b40c256c40134802550ece82add41da045f807d7
NoSocksForYou <kaappo.raivio@gmail.com>
2018-10-21 15:45:26 +0300

    autocommit

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from ev3dev import ev3
 import time
 
 print("trying to get motor handle")
-motor = ev3.Motor("outA")
-print("got ")
+
+
 # motor.run_forever()
 m = ev3.Motor('outA')
-
-# m.run_to_rel_pos(position_sp=360, speed_sp=900, stop_action="hold")
-# time.sleep(5)
-m.run_direct(speed_sp=900, stop_action="hold")
+m.run_forever(speed_sp=900)
+time.sleep(5)
+m.stop(stop_action="coast")
 time.sleep(5)
